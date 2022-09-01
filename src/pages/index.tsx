fe3286fd942d5990c86ff3e6c9c8d707e7c7da9d
Piyaphat Pinyo <remove158@gmail.com>
2022-09-02 02:41:22 +0700

    test:cloudflare pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-	return <div className="bg-red-200">Hello Next.js; I'm CU-REG dev</div>;
+	return <div className="bg-red-200">Hello Next.js; I am plangton</div>;
 };
 
 export default Home;
