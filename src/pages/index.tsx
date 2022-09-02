5f04d68ecc42b0953f44948a55fc0384c494c417
Piyaphat Pinyo <remove158@gmail.com>
2022-09-02 12:11:23 +0700

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-	return <div className="bg-red-200">Hello Next.js; I am plangton</div>;
+	return <div className="bg-red-200">Test </div>;
 };
 
 export default Home;
