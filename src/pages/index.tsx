1638e24e8c804a7523e64d06d93cb3ed1aed5634
plangton <remove158@gmail.com>
2022-10-21 22:17:40 +0700

    add: global style twin macro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,25 @@ import type { NextPage } from "next";
 import tw from "twin.macro";
 
 const Head = tw.div`
-	bg-red-200 
-	hover:(ring ring-purple-200 bg-purple-400 border-purple-200)
-	md:(bg-purple-300)
+	bg-purple-300 grid grid-cols-1 gap-2 p-2
+	md:(bg-purple-300 grid-cols-3)
+`;
+
+const Item = tw.div`
+	bg-purple-600 h-24 
+	hover:(bg-purple-400 transition-all scale-125)
 `;
 const Home: NextPage = () => {
-	return <Head>asdfasdf</Head>;
+	return (
+		<Head>
+			<Item />
+			<Item />
+			<Item />
+			<Item />
+			<Item />
+			<Item />
+		</Head>
+	);
 };
 
 export default Home;
