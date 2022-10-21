835ef3d4459c32f041a4272e256bc2a7ffd64570
plangton <remove158@gmail.com>
2022-10-21 22:24:03 +0700

    add: example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import type { NextPage } from "next";
 import tw from "twin.macro";
 
 const Head = tw.div`
-	bg-purple-300 grid grid-cols-1 gap-2 p-2
+	bg-purple-300 grid grid-cols-1 gap-2 p-2 
 	md:(bg-purple-300 grid-cols-3)
 `;
 
 const Item = tw.div`
-	bg-purple-600 h-24 
-	hover:(bg-purple-400 transition-all scale-125)
+	bg-purple-600 h-24 rounded-3xl transition-all duration-100
+	hover:(bg-purple-400 rounded-md)
 `;
 const Home: NextPage = () => {
 	return (
