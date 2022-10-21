864c8b02e872d9d534ea429536a139ec075e9b5a
plangton <remove158@gmail.com>
2022-10-21 21:47:40 +0700

    init: twin.macro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import tw from "twin.macro";
 
+const Head = tw.div`
+	bg-red-200 
+	hover:(ring ring-purple-200 bg-purple-400 border-purple-200)
+	md:(bg-purple-300)
+`;
 const Home: NextPage = () => {
-	return <div className="bg-red-200">Ready</div>;
+	return <Head>asdfasdf</Head>;
 };
 
 export default Home;
