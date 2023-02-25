28866052971b54e794a5f7240cfca01771c72234
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-02-25 11:12:54 +0700

    [setup] pages in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-import type { NextPage } from "next";
-import tw from "twin.macro";
+import type { NextPage } from 'next'
+import tw from 'twin.macro'
 
 const Head = tw.div`
-	bg-purple-300 grid grid-cols-1 gap-2 p-2 
+	bg-purple-300 grid grid-cols-1 gap-2 p-2
 	md:(bg-purple-300 grid-cols-3)
-`;
+`
 
 const Item = tw.div`
 	bg-purple-600 h-24 rounded-3xl transition-all duration-100
 	hover:(bg-purple-400 rounded-md)
-`;
+`
 const Home: NextPage = () => {
-	return (
-		<Head>
-			<Item />
-			<Item />
-			<Item />
-			<Item />
-			<Item />
-			<Item />
-		</Head>
-	);
-};
+  return (
+    <Head>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </Head>
+  )
+}
 
-export default Home;
+export default Home
---
+++ src/views/Request/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+// const Head = tw.div`
+// 	bg-purple-300 grid grid-cols-1 gap-2 p-2
+// 	md:(bg-purple-300 grid-cols-3)
+// `
+
+// const Item = tw.div`
+// 	bg-purple-600 h-24 rounded-3xl transition-all duration-100
+// 	hover:(bg-purple-400 rounded-md)
+// `
+const RequestPage: NextPage = () => {
+  return <>Request Page</>
+}
+
+export default RequestPage
