3990353bb44eae1e66ac45b90a38a6fa2a408b7d
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-03-04 15:38:51 +0700

    [edit, create - wip] main layout, request page

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -16,8 +16,8 @@ const MainLayout: FC<IMainLayoutProps> = ({ children, header, studentId }) => {
       <SideMenu />
       <div tw="flex flex-col w-full min-h-screen p-[50px]">
         <div css={[tw`mb-8`]}>
-          <div tw="text-h1 font-h1 text-cu-pink">{header}</div>
-          <div tw="mt-2 text-h2 font-h2 text-black pl-4">{`เลขประจำตัว: ${studentId}`}</div>
+          {header && <div tw="text-h1 font-h1 text-cu-pink">{header}</div>}
+          {studentId && <div tw="mt-2 text-h2 font-h2 text-black pl-4">{`เลขประจำตัว: ${studentId}`}</div>}
         </div>
         <div className="flex flex-col grow">{children}</div>
         <MainFooter />
--- src/views/Request/index.tsx
+++ src/views/Request/index.tsx
@@ -1,16 +1,8 @@
+import MainLayout from 'layouts/MainLayout'
 import type { NextPage } from 'next'
 
-// const Head = tw.div`
-// 	bg-purple-300 grid grid-cols-1 gap-2 p-2
-// 	md:(bg-purple-300 grid-cols-3)
-// `
-
-// const Item = tw.div`
-// 	bg-purple-600 h-24 rounded-3xl transition-all duration-100
-// 	hover:(bg-purple-400 rounded-md)
-// `
 const RequestPage: NextPage = () => {
-  return <>Request Page</>
+  return <MainLayout header="ยื่นคำร้องเพื่อขอเอกสารสำคัญทางการศึกษา" studentId="6231354721" />
 }
 
 export default RequestPage
