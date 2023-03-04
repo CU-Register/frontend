ff1e138bd224b6ae006c06b8b53e0d24097779dd
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-03-04 15:27:30 +0700

    [create, complete] profile page ui

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -8,8 +8,8 @@ import ProfileDetails from './components/ProfileDetails'
 const ProfilePage: NextPage = () => {
   return (
     <MainLayout header="ข้อมูลทั่วไปและส่วนบุคคล" studentId="6231354721">
-      <div tw="bg-cu-copper flex justify-between">
-        <div tw="bg-cu-pinkLd flex flex-col justify-between">
+      <div tw=" flex justify-between">
+        <div tw=" flex flex-col justify-between">
           <div tw="flex gap-12">
             <div tw="font-h2 text-h2 text-black">ชื่อ-นามสกุล</div>
             <div tw="flex flex-col font-h1 text-h1 text-black">
@@ -21,14 +21,14 @@ const ProfilePage: NextPage = () => {
             <PinkButton text="แก้ไข" />
           </div>
         </div>
-        <div tw="bg-cu-gold">
+        <div>
           <div tw="w-[129px] h-[178px] relative">
             <Image src="/assets/mock-profile-pic.png" layout="fill" alt="chula-icon" />
           </div>
         </div>
       </div>
-      <div tw="mt-6 bg-green-300 grid gap-1" className="grid-cols-[1fr_2px_1fr]">
-        <div tw="bg-normal-100 flex flex-col gap-2">
+      <div tw="mt-6  grid gap-3" className="grid-cols-[1fr_2px_1fr]">
+        <div tw=" flex flex-col gap-2">
           <ProfileDetails label="ระบบการศึกษา" value="ทวิภาค" />
           <ProfileDetails label="ระดับการศึกษา" value="ปริญญาตรี" />
           <ProfileDetails label="หลักสูตร" value="วิศวกรรมศาสตร์บัณฑิต" valueColor="text-gray" />
@@ -40,7 +40,7 @@ const ProfilePage: NextPage = () => {
           </div>
         </div>
         <VerticalDivider />
-        <div tw="bg-interactive-100 flex flex-col gap-2">
+        <div tw="flex flex-col gap-2">
           <ProfileDetails label="ที่อยู่" value="254 ถนนพญาไท แขวงวังใหม่ เขตปทุมวัน กรุงเทพมหานคร 10330" />
           <ProfileDetails label="เบอร์โทรศัพท์" value="061 447 5178" />
           <ProfileDetails label="อีเมล์" value="sudlhorpromptleaw@gmail.com" />
