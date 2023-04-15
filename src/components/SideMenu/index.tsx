7f8d048efc2b143e1ed1c29e90b3a49cd0bf776b
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-04-15 17:17:40 +0700

    [create] toggle display protected and common sidemenu buttons

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,4 +1,4 @@
-import ROUTES from 'constants/Routes'
+import { COMMON_ROUTES, PROTECTED_ROUTES } from 'constants/Routes'
 import useAuth from 'hooks/useAuth'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -10,8 +10,15 @@ import SideMenuButton from './SideMenuButton'
 interface ISideMenu {}
 const SideMenu: FC<ISideMenu> = () => {
   const { logout } = useAuth()
-
   const router = useRouter()
+
+  const hideProtectedRoutesButton = () => {
+    return Object.values(COMMON_ROUTES).includes(router.pathname)
+  }
+
+  const hideCommonRoutesButton = () => {
+    return !Object.values(COMMON_ROUTES).includes(router.pathname)
+  }
 
   const homePageButtonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
@@ -53,31 +60,49 @@ const SideMenu: FC<ISideMenu> = () => {
           <div tw="text-h1 font-h1 text-white">CUADRS</div>
         </div>
         <div tw="mt-8 flex flex-col items-start gap-5">
-          <SideMenuButton text="หน้าหลัก" onClick={homePageButtonHandler} isFocused={router.pathname === '/'} />
+          {/* Common Routes */}
+          <SideMenuButton
+            text="เข้าสู่ระบบ"
+            isFocused={router.pathname === COMMON_ROUTES.LOGIN}
+            hidden={hideCommonRoutesButton()}
+          />
+
+          {/* Protected Routes */}
+          <SideMenuButton
+            text="หน้าหลัก"
+            onClick={homePageButtonHandler}
+            isFocused={router.pathname === '/'}
+            hidden={hideProtectedRoutesButton()}
+          />
           <SideMenuButton
             text="ยิื่นคำร้อง"
             onClick={requestPageButtonHandler}
-            isFocused={router.pathname.includes(ROUTES.REQUEST)}
+            isFocused={router.pathname.includes(PROTECTED_ROUTES.REQUEST)}
+            hidden={hideProtectedRoutesButton()}
           />
           <SideMenuButton
             text="โครงร่างคำร้อง"
             onClick={draftPageButtonHandler}
-            isFocused={router.pathname.includes(ROUTES.DRAFT)}
+            isFocused={router.pathname.includes(PROTECTED_ROUTES.DRAFT)}
+            hidden={hideProtectedRoutesButton()}
           />
           <SideMenuButton
             text="ตรวจสอบสถานะ"
             onClick={statusPageButtonHandler}
-            isFocused={router.pathname.includes(ROUTES.STATUS)}
+            isFocused={router.pathname.includes(PROTECTED_ROUTES.STATUS)}
+            hidden={hideProtectedRoutesButton()}
           />
           <SideMenuButton
             text="ข้อมูลผู้ใช้"
             onClick={profilePageButtonHandler}
-            isFocused={router.pathname.includes(ROUTES.PROFILE)}
+            isFocused={router.pathname.includes(PROTECTED_ROUTES.PROFILE)}
+            hidden={hideProtectedRoutesButton()}
           />
           <SideMenuButton
             text="ออกจากระบบ"
             onClick={logoutPageButtonHandler}
-            isFocused={router.pathname.includes(ROUTES.LOGOUT)}
+            isFocused={router.pathname.includes(PROTECTED_ROUTES.LOGOUT)}
+            hidden={hideProtectedRoutesButton()}
           />
         </div>
       </div>
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import ROUTES from 'constants/Routes'
+import { COMMON_ROUTES, PROTECTED_ROUTES } from 'constants/Routes'
 import { useRouter } from 'next/router'
 import authService from 'services/auth.service'
 import { useAuthStore } from 'stores/auth.stores'
@@ -13,17 +13,17 @@ const useAuth = () => {
       const { accessToken, refreshToken } = result
       authStore.setAccessToken(accessToken)
       localStorage.setItem('cuadrs-refreshToken', refreshToken)
-      router.push(ROUTES.HOME)
+      router.push(PROTECTED_ROUTES.HOME)
     } catch (error) {
       console.error(error)
-      router.push(ROUTES.LOGIN)
+      router.push(COMMON_ROUTES.LOGIN)
     }
   }
 
   const logout = () => {
     localStorage.removeItem('cuadrs-refreshToken')
     authStore.setAccessToken(null)
-    router.replace(ROUTES.LOGOUT)
+    router.replace(PROTECTED_ROUTES.LOGOUT)
   }
 
   return { login, logout }
