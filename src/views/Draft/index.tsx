d8cca981a80a70bd319cf46eea458baea1d6a697
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-02 12:31:00 +0700

    [edit] hide search and query section at draft and status page

--- src/views/Draft/index.tsx
+++ src/views/Draft/index.tsx
@@ -2,12 +2,11 @@ import withAuth from 'components/Auth/withAuth'
 import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
 import RequestDraftSection from './sections/RequestDraftSection'
-import SearchAndQuerySection from './sections/SearchAndQuerySection'
 
 const DraftPage: NextPage = () => {
   return (
     <MainLayout header="แก้ไขโครงร่างคำร้อง">
-      <SearchAndQuerySection />
+      {/* <SearchAndQuerySection /> */}
       <RequestDraftSection />
     </MainLayout>
   )
--- src/views/Status/pages/index.tsx
+++ src/views/Status/pages/index.tsx
@@ -3,11 +3,10 @@ import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
 import 'twin.macro'
 import RequestStatusSection from '../sections/RequestStatusSection'
-import SearchAndQuerySection from '../sections/SearchAndQuerySection'
 const StatusPage: NextPage = () => {
   return (
     <MainLayout header="ตรวจสอบสถานะการยื่นคำร้อง">
-      <SearchAndQuerySection />
+      {/* <SearchAndQuerySection /> */}
       <RequestStatusSection />
     </MainLayout>
   )
