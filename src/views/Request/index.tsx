ef3b4aa00fe560da72cb54298313d691b89ea9a0
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-02 09:02:58 +0700

    [refactor] hide SearchAndQuery bar, renaming files

--- src/views/Request/index.tsx
+++ src/views/Request/index.tsx
@@ -3,12 +3,11 @@ import MainLayout from 'layouts/MainLayout'
 import type { NextPage } from 'next'
 import 'twin.macro'
 import RequestListSection from './sections/RequestListSection'
-import SearchAndQuerySection from './sections/SearchAndQuerySection'
 
 const RequestPage: NextPage = () => {
   return (
     <MainLayout header="ยื่นคำร้องเพื่อขอเอกสารสำคัญทางการศึกษา">
-      <SearchAndQuerySection />
+      {/* <SearchAndQuerySection /> */}
       <RequestListSection />
     </MainLayout>
   )
