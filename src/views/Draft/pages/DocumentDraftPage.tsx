f7920b4e03a7808d96e28a6fdda2f6084667666b
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-03 15:27:43 +0700

    [edit] document draft page

--- src/views/Draft/pages/DocumentDraftPage.tsx
+++ src/views/Draft/pages/DocumentDraftPage.tsx
@@ -1,4 +1,7 @@
 import withAuth from 'components/Auth/withAuth'
+import VerticalDivider from 'components/Dividers/VerticalDivider'
+import NeutralButton from 'components/NeutralButton'
+import PinkButton from 'components/PinkButton'
 import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
 import 'twin.macro'
@@ -6,16 +9,19 @@ import 'twin.macro'
 const DocumentDraftPage: NextPage = () => {
   return (
     <MainLayout header="แก้ไขโครงร่างคำร้อง">
-      <div tw="text-h1 font-h1 text-black bg-cu-copper">คำร้องขอลาออก (จท32)</div>
-      <div tw="bg-cu-gold px-4 py-2 flex flex-col flex-1 mb-4">
-        <div tw="bg-cu-pinkLd flex justify-between text-black text-h2 font-h2">
-          <div tw="bg-negative-100">กรุณากรอกข้อมูลที่ไม่ถูกสีระบายทับ</div>
-          <div tw="bg-green-300">แก้ไขล่าสุด: 12/4/2565, 12:23 น.</div>
+      <div tw="text-h1 font-h1 text-black">คำร้องขอลาออก (จท32)</div>
+      <div tw="px-4 py-2 flex flex-col flex-1 mb-4">
+        <div tw="flex justify-between text-black text-h2 font-h2">
+          <div>กรุณากรอกข้อมูลที่ไม่ถูกสีระบายทับ</div>
+          <div>แก้ไขล่าสุด: 12/4/2565, 12:23 น.</div>
         </div>
-        <div tw="bg-interactive-100 flex-1" className="grow">
-          for pdf rendering
+        <div tw="flex-1 flex justify-center items-center overflow-auto">for document draft pdf</div>
+        <div tw="flex justify-end gap-4">
+          <NeutralButton text="ยกเลิกเอกสาร" iconSrc="/assets/delete-document-icon.png" />
+          <VerticalDivider />
+          <NeutralButton text="บันทึกและย้อนกลับ" />
+          <PinkButton text="ไปต่อ" />
         </div>
-        <div tw="bg-normal-100">for user actions</div>
       </div>
     </MainLayout>
   )
