7378494e6a4ec909fdeb4a35d4e11c7bcabc8adf
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-09 14:42:05 +0700

    [refactor] renaming component request status section > document status section

--- src/views/Status/pages/index.tsx
+++ src/views/Status/pages/index.tsx
@@ -4,7 +4,7 @@ import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
 import { useEffect } from 'react'
 import 'twin.macro'
-import RequestStatusSection from '../sections/RequestStatusSection'
+import DocumentStatusSection from '../sections/DocumentStatusSection'
 const StatusPage: NextPage = () => {
   const { fetchHistoryDocuments } = useDocument()
 
@@ -15,7 +15,7 @@ const StatusPage: NextPage = () => {
   return (
     <MainLayout header="ตรวจสอบสถานะการยื่นคำร้อง">
       {/* <SearchAndQuerySection /> */}
-      <RequestStatusSection />
+      <DocumentStatusSection />
     </MainLayout>
   )
 }
--- src/views/Status/sections/DocumentStatusSection/index.tsx
+++ src/views/Status/sections/DocumentStatusSection/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import 'twin.macro'
 import DocumentStatusTable from 'views/Status/components/DocumentStatusTable'
-const RequestStatusSection: FC = () => {
+const DocumentStatusSection: FC = () => {
   return (
     <div tw="my-6 flex flex-col gap-5 flex-1 overflow-auto">
       <div tw="text-body font-body text-gray">สามารถคลิ้กที่ชื่อคำร้อง เพื่อดูข้อมูลเพิ่มเติ่มอย่างละเอียด</div>
@@ -12,4 +12,4 @@ const RequestStatusSection: FC = () => {
   )
 }
 
-export default RequestStatusSection
+export default DocumentStatusSection
