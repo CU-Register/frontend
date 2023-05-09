66fb9be18953f29cddfac74b2bf240a901940636
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-09 22:54:02 +0700

    [hotfeat] bind history document to table

--- src/views/Home/components/HistoryDocumentTable/index.tsx
+++ src/views/Home/components/HistoryDocumentTable/index.tsx
@@ -1,17 +1,18 @@
 import { FC } from 'react'
 import { useDocumentStore } from 'stores/document.store'
 import 'twin.macro'
+import { dateFormatter } from 'utils/formats'
 
 interface IHistoryDocumentTableProps {}
 const HistoryDocumentTable: FC<IHistoryDocumentTableProps> = () => {
   const { historyDocuments } = useDocumentStore()
+
   return (
     <>
       {(!historyDocuments || historyDocuments.length === 0) && (
         <div tw="text-h2 font-h2 text-gray flex justify-center">ไม่พบประวัติการยื่นคำร้อง</div>
       )}
       {historyDocuments && historyDocuments.length > 0 && (
-        // TODO map history document fields to table body
         <table tw="table-auto w-full">
           <thead>
             <tr>
@@ -26,22 +27,16 @@ const HistoryDocumentTable: FC<IHistoryDocumentTableProps> = () => {
               </th>
             </tr>
           </thead>
-          <tbody tw="overflow-auto cursor-pointer">
-            <tr>
-              <td tw="font-h2 text-h2 text-black">คำร้องที่ขอเข้าสังกัดหรือเปลี่ยนสังกัด</td>
-              <td tw="font-h2 text-h2 text-black">14/12/2012</td>
-              <td tw="font-h2 text-h2 text-gray">ถูกยกเลิก</td>
-            </tr>
-            <tr>
-              <td tw="font-h2 text-h2 text-black">คำร้องแนะนำที่ 2</td>
-              <td tw="font-h2 text-h2 text-black">13/12/2012</td>
-              <td tw="font-h2 text-h2 text-cu-pink">เสร็จสิ้น</td>
-            </tr>
-            <tr>
-              <td tw="font-h2 text-h2 text-black">คำร้องขอลาออก</td>
-              <td tw="font-h2 text-h2 text-black">12/12/2012</td>
-              <td tw="font-h2 text-h2 text-black">ระหว่างการดำเนินการ</td>
-            </tr>
+          <tbody tw="overflow-auto cursor-pointer font-h2 text-h2 text-black">
+            {historyDocuments.map((historyDocument, index) => {
+              return (
+                <tr key={index} tw=" font-h2 text-h2 text-black">
+                  <td tw="">{historyDocument.template.title.th}</td>
+                  <td tw="">{dateFormatter(historyDocument.createdAt)}</td>
+                  <td tw="">{historyDocument.status}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       )}
