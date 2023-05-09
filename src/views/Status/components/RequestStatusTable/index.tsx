bee54bf9524fc8078e9bd5066ce27a1ef535dc11
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-09 14:32:09 +0700

    [edit] request status table: state binding

--- src/views/Status/components/RequestStatusTable/index.tsx
+++ src/views/Status/components/RequestStatusTable/index.tsx
@@ -1,43 +1,55 @@
-import { RequestStatus } from 'enums/RequestStatus'
-import mockRequests from 'mocks/Requests'
+import { UserRole } from 'enums/UserRole'
 import { FC } from 'react'
+import { useDocumentStore } from 'stores/document.store'
+import { useProfileStore } from 'stores/profile.store'
 import 'twin.macro'
-import tw from 'twin.macro'
-import { dateFormatter } from 'utils/formats'
+import { dateFormatter, fullNameFormatter } from 'utils/formats'
 
 interface IRequestStatusTableProps {}
 const RequestStatusTable: FC<IRequestStatusTableProps> = () => {
+  const { historyDocuments } = useDocumentStore()
+  const { userProfile } = useProfileStore()
+
   return (
-    <table tw="table-auto w-full">
-      <thead tw="h-[40px]sticky" className="text-start align-top">
-        <tr tw="text-gray font-h2 text-h2">
-          <th className="text-start">ชื่อคำร้อง</th>
-          <th className="text-start">เลขที่จท.</th>
-          <th className="text-start">วันที่ยื่นคำร้อง</th>
-          <th className="text-start">สถานะการดำเนินการ</th>
-        </tr>
-      </thead>
-      <tbody>
-        {mockRequests.map((request, index) => {
-          return (
-            <tr key={index} tw="text-black font-h2 text-h2" className="group">
-              <td tw="group-hover:text-cu-pink">{request.requestName}</td>
-              <td tw="group-hover:text-cu-pink">{request.requestNumber}</td>
-              <td tw="group-hover:text-cu-pink">{dateFormatter(request.createdAt)}</td>
-              <td
-                css={[
-                  request.status === RequestStatus.CANCELED && tw`text-gray`,
-                  request.status === RequestStatus.COMPLETED && tw`text-cu-pink`,
-                ]}
-                tw="group-hover:text-cu-pink"
-              >
-                {request.status}
-              </td>
+    <>
+      {(!historyDocuments || historyDocuments.length === 0) && (
+        <div tw="text-h2 font-h2 text-gray flex justify-center h-full items-center">ไม่พบประวัติการยื่นคำร้อง</div>
+      )}
+      {historyDocuments && historyDocuments.length > 0 && (
+        <table tw="table-auto w-full">
+          <thead tw="h-[40px]sticky" className="text-start align-top">
+            <tr tw="text-gray font-h2 text-h2">
+              <th className="text-start">ชื่อคำร้อง</th>
+              <th className="text-start">เลขที่จท.</th>
+              {userProfile?.role !== UserRole.STUDENT && <th className="text-start">ผ​ู้เขียนคำร้อง</th>}
+              <th className="text-start">วันที่ยื่นคำร้อง</th>
+              {userProfile?.role !== UserRole.STUDENT && <th className="text-start">การดำเนินการ</th>}
+              <th className="text-start">สถานะ</th>
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {historyDocuments.map((historyDocument, index) => {
+              return (
+                <tr key={index} tw="text-black font-h2 text-h2" className="group">
+                  <td tw="group-hover:text-cu-pink">{historyDocument.template.title.th}</td>
+                  <td tw="group-hover:text-cu-pink">{historyDocument.template.templateType}</td>
+                  {userProfile?.role !== UserRole.STUDENT && (
+                    <td tw="group-hover:text-cu-pink">
+                      {fullNameFormatter(historyDocument.creator.firstname.th, historyDocument.creator.lastname.th)}
+                    </td>
+                  )}
+                  <td tw="group-hover:text-cu-pink">{dateFormatter(historyDocument.grantedAt)}</td>
+                  {userProfile?.role !== UserRole.STUDENT && (
+                    <td tw="group-hover:text-cu-pink">{historyDocument.action}</td>
+                  )}
+                  <td tw="group-hover:text-cu-pink">{historyDocument.status}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
   )
 }
 
--- src/views/Status/pages/index.tsx
+++ src/views/Status/pages/index.tsx
@@ -1,9 +1,17 @@
 import withAuth from 'components/Auth/withAuth'
+import useDocument from 'hooks/useDocument'
 import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
+import { useEffect } from 'react'
 import 'twin.macro'
 import RequestStatusSection from '../sections/RequestStatusSection'
 const StatusPage: NextPage = () => {
+  const { fetchHistoryDocuments } = useDocument()
+
+  useEffect(() => {
+    fetchHistoryDocuments()
+  }, [])
+
   return (
     <MainLayout header="ตรวจสอบสถานะการยื่นคำร้อง">
       {/* <SearchAndQuerySection /> */}
