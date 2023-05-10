f942deabec9f865895c70049fac7c909631a3f20
osm <remove158@gmail.com>
2023-05-10 22:30:00 +0700

    add feature to display document status and document action (#27)
    * add: status and action component
    
    * fix: datatype action and status

---
+++ src/components/DocumentHistroy/action.tsx
@@ -0,0 +1,22 @@
+import { DocumentHistoryActionEnum } from 'enums/Document'
+import { FC } from 'react'
+import tw from 'twin.macro'
+
+interface IDocumentHistoryAction {
+  action?: DocumentHistoryActionEnum
+}
+const DocumentHistoryAction: FC<IDocumentHistoryAction> = ({ action: status }) => {
+  const statusMapper: Record<DocumentHistoryActionEnum, string> = {
+    [DocumentHistoryActionEnum.APPROVED]: 'สร้างเอกสาร',
+    [DocumentHistoryActionEnum.CREATED]: 'ส่งต่อ',
+    [DocumentHistoryActionEnum.DECLINED]: 'ยกเลิก',
+  }
+
+  return (
+    <div css={[`text-black`, status === DocumentHistoryActionEnum.DECLINED && tw`text-cu-grey`]}>
+      {status && statusMapper[status]}
+    </div>
+  )
+}
+
+export default DocumentHistoryAction
