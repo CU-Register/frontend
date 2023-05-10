9afc360f61727ba4a4c502f2483ba2ed463f6ef6
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2023-05-10 14:53:19 +0700

    [create] admin home page

--- src/views/Admin/pages/index.tsx
+++ src/views/Admin/pages/index.tsx
@@ -1,12 +1,22 @@
+import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import withAuth from 'components/Auth/withAuth'
 import MainLayout from 'layouts/MainLayout'
 import { NextPage } from 'next'
 import 'twin.macro'
 
 const AdminHomePage: NextPage = () => {
+  const createNewTemplateHandler = (event: React.MouseEvent<HTMLDivElement>) => {}
   return (
-    <MainLayout>
-      <div tw="bg-cu-copper">admin home page</div>
+    <MainLayout header="จัดการเทมเพลตเอกสาร">
+      <div tw="mt-5 ml-10 flex-1 mb-3 overflow-auto">
+        <div tw="flex gap-4 items-center">
+          <div tw="text-h2 font-h2">สร้างเทมเพลตเอกสารใหม่:</div>
+          <div tw="text-h1 text-cu-pink" onClick={(event) => createNewTemplateHandler(event)}>
+            <FontAwesomeIcon icon={faFileCirclePlus} />
+          </div>
+        </div>
+      </div>
     </MainLayout>
   )
 }
